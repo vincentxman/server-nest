4d22302024b3e43607ba8d87faba693761705376
vincent <vincentxman@hotmail.com>
2019-10-30 01:37:12 +0800

    move sandbox

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TstController } from './tst/tst.controller';
+import { TstController } from './controller/sandbox/tst/tst.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
-import { GrQlModule } from './gr-ql/gr-ql.module';
+import { GrQlModule } from './resolver/sandbox/gr-ql/gr-ql.module';
 import config from './config/keys';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
 
-import { ItemsModule } from './items/items.module';
-import { GrCatsModule } from './gr-cats/gr-cats.module';
+import { ItemsModule } from './controller/sandbox/tems/items.module';
+import { GrCatsModule } from './resolver/sandbox/gr-cats/gr-cats.module';
 
 @Module({
   imports: [
