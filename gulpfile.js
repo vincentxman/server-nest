dabddc60f2fb5b077a9d421d57ae9ea73ea9ab66
vincent <vincentxman@hotmail.com>
2019-11-07 15:05:10 +0800

    docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var clean = require('gulp-clean');
 var FtpDeploy = require("ftp-deploy");
 
 var ftpDeploy = new FtpDeploy();
-var config = {
+var configDist = {
     user: "audioprint",
     password: "12@*aZs&f=-23@%^@sddAZ",
     host: "o-pen.com.cn",
@@ -27,8 +27,8 @@ var config = {
 
 function ftpDistDir() {
     return ftpDeploy
-        .deploy(config)
-        .then(res => console.log("finished:", res))
+        .deploy(configDist)
+        .then(res => console.log("finished /dist/:", res))
         .catch(err => console.log(err));
 }
 
@@ -37,6 +37,36 @@ function cleanDistDir() {
     return src('dist', {read: false})
     .pipe(clean());
 }
+// ------------------------------------------------
+var configDocs = {
+    user: "audioprint",
+    password: "12@*aZs&f=-23@%^@sddAZ",
+    host: "o-pen.com.cn",
+    port: 21,
 
+    localRoot: __dirname + "/docs/",
+    remoteRoot: "/server-nest/docs/",
+
+    // this would upload everything except dot files   
+    include: ['*', '**/*'],
+
+    // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+    exclude: ['**/*.d.ts', '**/*.js.map', '**/*.bak', "node_modules/**", "node_modules/**/.*",],
+
+    // delete ALL existing files at destination before uploading, if true
+    deleteRemote: true,
+
+    // Passive mode is forced (EPSV command is not sent)
+    forcePasv: true
+};
+
+function ftpDocsDir() {
+    return ftpDeploy
+        .deploy(configDocs)
+        .then(res => console.log("finished /docs/:", res))
+        .catch(err => console.log(err));
+}
+
+//-------------------------------------------------
 exports.cleanDistDir = cleanDistDir;
-exports.server2Ftp = series(ftpDistDir);
+exports.server2Ftp = series(ftpDocsDir, ftpDistDir);
